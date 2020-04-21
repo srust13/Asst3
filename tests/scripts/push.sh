eff1f08ccf238ff7d4ea254a5670b15654e917f2
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2020-04-21 15:37:24 -0700

    updated push test

--- tests/scripts/push.sh
+++ tests/scripts/push.sh
@@ -8,13 +8,13 @@ pid=$!
 # start client
 cd ../client
 ../../bin/WTF push huffman_dir
-cp huffman_dir/.Manifest ../server/huffman_dir
 echo "abcd" > huffman_dir/file1
 echo "somethinnew" > huffman_dir/file2
 ../../bin/WTF add huffman_dir huffman_dir/file2
 ../../bin/WTF remove huffman_dir huffman_dir/file3
 ../../bin/WTF commit huffman_dir
 ../../bin/WTF push huffman_dir
+rm huffman_dir/file3
 
 # kill server
 kill -INT $pid &2>/dev/null
@@ -24,4 +24,4 @@ result="$(cat huffman_dir/.Manifest)"
 expected='2 huffman_dir
 - F5AC8127B3B6B85CDC13F237C6005D80 1 huffman_dir/file1
 - F9B11CF2A505297A82D209FB28B62FE2 0 huffman_dir/file2'
-[[ "$result" == "$expected" ]]
+[[ "$result" == "$expected" ]] && diff -qr huffman_dir ../server/huffman_dir
