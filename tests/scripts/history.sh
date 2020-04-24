874b68b72f6ca32590df60d0ddaa99a62be23678
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2020-04-24 16:44:54 -0700

    fixed history test

--- tests/scripts/history.sh
+++ tests/scripts/history.sh
@@ -8,7 +8,9 @@ pid=$!
 # start client
 cd ../client
 result="$(../../bin/WTF history huffman_dir)"
-expected='1
+expected='Server connected
+
+1
 A C84F8A070A28B21C1D84F6DDED7EB103 0 huffman_dir/file1
 A 1F2252AC5146C4C0EECE85C4617C5D4F 0 huffman_dir/file3
 2
@@ -20,7 +22,9 @@ A 7A9993E80A235A3632DEEC9A6ED2EDA1 0 huffman_dir/file3
 4
 D F5AC8127B3B6B85CDC13F237C6005D80 1 huffman_dir/file1
 5
-M 1FFB2614C326255B00E3D94DDED89DAF 1 huffman_dir/file2'
+M 1FFB2614C326255B00E3D94DDED89DAF 1 huffman_dir/file2
+
+Command completed successfully'
 
 # kill server
 kill -INT $pid 2>/dev/null
